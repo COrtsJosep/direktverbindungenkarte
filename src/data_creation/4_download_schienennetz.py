3985f7e7d04cc6facb59a7edc42f9978423387a6
Josep <corts.josep@gmail.com>
2026-04-24 18:48:52 +0200

    fixed two missing connections in the network

--- src/data_creation/4_download_schienennetz.py
+++ src/data_creation/4_download_schienennetz.py
@@ -1,3 +1,4 @@
+import shapely
 import geopandas as gpd
 from pathlib import Path
 cd = Path(__file__).parent
@@ -18,7 +19,6 @@ gdf_ka = (  # schienennetz kanten
     )
     .set_index(['from_node_object_id', 'to_node_object_id'])
 )
-gdf_ka.to_file(cd.parent / 'assets' / 'schienennetz_kanten.geojson')
 
 gdf_kn = (  # schienennetz knoten
     gpd
@@ -33,4 +33,23 @@ gdf_kn = gdf_kn.loc[  # only nodes that are the start or end of a railway edge
     (gdf_kn.index.isin(gdf_ka.index.get_level_values('from_node_object_id')))
     | (gdf_kn.index.isin(gdf_ka.index.get_level_values('to_node_object_id')))
 ]
+
+### need to add two missing connections, between two points close to Wollishofen,
+### and two points close to Reichenau-Bonaduz
+wollishofen_line = shapely.LineString(
+    [
+        gdf_kn.loc['7dc1de4a-0d99-45b4-a148-16c0c85a4be2', 'geometry'],
+        gdf_kn.loc['4fcd2c41-6dc6-46cd-be98-6b6d3c824ea0', 'geometry'],
+    ]
+)
+reichenau_line = shapely.LineString(
+    [
+        gdf_kn.loc['c2533fbd-78da-4ead-92e4-12c41676c47d', 'geometry'],
+        gdf_kn.loc['5ce3edab-8b7d-433f-936f-d1c11466defd', 'geometry'],
+    ]
+)
+gdf_ka.loc[('7dc1de4a-0d99-45b4-a148-16c0c85a4be2', '4fcd2c41-6dc6-46cd-be98-6b6d3c824ea0'), :] = [wollishofen_line.length, wollishofen_line]
+gdf_ka.loc[('c2533fbd-78da-4ead-92e4-12c41676c47d', '5ce3edab-8b7d-433f-936f-d1c11466defd'), :] = [reichenau_line.length, reichenau_line]
+
+gdf_ka.to_file(cd.parent / 'assets' / 'schienennetz_kanten.geojson')
 gdf_kn.to_file(cd.parent / 'assets' / 'schienennetz_knoten.geojson')
--- src/data_creation/run_all.sh
+++ src/data_creation/run_all.sh
@@ -1,6 +1,11 @@
 #! /usr/bin/bash
+echo 'Running 1_download_dienststellen.py'
 python 1_download_dienststellen.py
+echo 'Running 2_download_istdaten.py'
 python 2_download_istdaten.py
+echo 'Running 3_clean_dienststellen.py'
 python 3_clean_dienststellen.py
+echo 'Running 4_download_schienennetz.py'
 python 4_download_schienennetz.py
+echo 'Running python 5_create_files_per_station.py'
 python 5_create_files_per_station.py
